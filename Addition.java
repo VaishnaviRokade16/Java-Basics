290cfbab2eda8d6dd4284adc687f97e431d813ff
Vaishnavi Rokade <rokadevaishnavi520@gmail.com>
2025-10-31 08:14:39 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 14;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
+
