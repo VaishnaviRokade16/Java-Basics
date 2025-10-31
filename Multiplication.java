2356de1a3021e66be1a68d7f58f7cb8011619d07
Vaishnavi Rokade <rokadevaishnavi520@gmail.com>
2025-10-31 08:16:33 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 5, b = 4;
+        int result = a * b;
+        System.out.println("Multiplication: " + result);
+    }
+}
+
